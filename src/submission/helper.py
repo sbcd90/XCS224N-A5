8f8f3c0166e39ef98ee2d1bac2ec248099acc966
Subhobrata Dey <sbcd90@gmail.com>
2022-12-09 18:47:53 -0800

    yet another commit

--- src/submission/helper.py
+++ src/submission/helper.py
@@ -22,6 +22,7 @@ def initialize_synthesizer_model(mconf):
     ### [part g]: Make some other model here
 
     ### START CODE HERE
+    attention_model = GPT(mconf)
     ### END CODE HERE
     return attention_model
 
